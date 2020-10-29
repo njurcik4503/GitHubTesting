332e9db36e3572dca90e245d541052f5fbfc4631
natalie <njurcik4503@stu.d214.org>
2020-10-29 08:55:07 -0500

    trying something

---
+++ GitHubTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTesting
+//
+//  Created by Natalie Jurcik on 10/20/20.
+//  Copyright © 2020 Natalie Jurcik. All rights reserved.
+// checking to see if it works
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
